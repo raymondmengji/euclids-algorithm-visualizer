406c5790677b56156e8e378e1b3adbd014bfeba1
Raymond Ji <raymondji@berkeley.edu>
2020-02-18 16:36:59 -0800

    added mousePressed

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -24,12 +24,14 @@ class Box {
 		this.index = index;
 		this.x = x;
 		this.y = y;
-		fill(255);
-		rect(this.x, this.y, boxSize, boxSize);
 	}
 
 	getIndex() {
 		return this.index;
+	}
+
+	setIndex(index) {
+		this.index = index;
 	}
 
 	getX() {
@@ -44,14 +46,35 @@ class Box {
 
 function setup() {
 	canvas = createCanvas(window.innerWidth, window.innerHeight);
+	grid = new Grid(16, 10);
 }
 
 function draw() {
+	// refresh background to white
 	background(255);
-	grid = new Grid(10, 20);
 
+	// creates grid boxes
+	fill(255);
+	rect(grid.getBox1().getX(), grid.getBox1().getY(), boxSize, boxSize);
+	rect(grid.getBox2().getX(), grid.getBox2().getY(), boxSize, boxSize);
+
+	// creates text for numbers
 	fill(0);
 	textSize(60);
 	text(grid.getBox1().getIndex(), (window.innerWidth-1.5*boxSize)/2, (window.innerHeight-boxSize/1.5)/2);
 	text(grid.getBox2().getIndex(), (window.innerWidth+boxSize/2)/2, (window.innerHeight-boxSize/1.5)/2);
 }
+
+function nextEuclid() {
+	if (grid.getBox2().getIndex() != 0) {
+		let box1Index = grid.getBox2().getIndex();
+		let box2Index = grid.getBox1().getIndex() % grid.getBox2().getIndex();
+		grid.getBox1().setIndex(box1Index);
+		grid.getBox2().setIndex(box2Index); 
+	}
+}
+
+function mousePressed() {
+	nextEuclid();
+}
+
